5a93699bdb222744fb8cfada2c4139a88dd450d1
pedrocriado <pedrocriadof2004@gmail.com>
2024-12-25 21:49:02 -0500

    created redis connection

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 require('dotenv').config({path: '../process.env'});
 
-async function connectToDatabase() {
-    await mongoose.connect(process.env.MONGO_URI)
+async function connectToMongoDB() {
+    await mongoose.connect(process.env.MONGO_URL)
         .then(() => {
-            console.log("You successfully connected to MongoDB using Mongoose!");
+            console.log("You successfully connected to MongoDB!");
         })
         .catch(error => {
             console.error("error connecting to MongoDB:", error);
         });
 }
 
-module.exports = connectToDatabase;
+module.exports = connectToMongoDB;
