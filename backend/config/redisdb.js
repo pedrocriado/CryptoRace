15545574a7a0208021706fba5154ea57ca059ad3
pedrocriado <pedrocriadof2004@gmail.com>
2024-12-25 21:49:02 -0500

    created redis connection

---
+++ backend/config/redisdb.js
@@ -0,0 +1,21 @@
+const redis = require('redis');
+
+require('dotenv').config({path: '../process.env'});
+
+async function connectToRedisDB() {
+    const client = redis.createClient({
+        url: process.env.REDIS_URL
+    });
+
+    client.on('error', err => console.log('Redis Client Error', err));
+
+    await client.connect()
+    .then(() => {
+        console.log("You successfully connected to Redis!");
+    })
+    .catch(error => {
+        console.error("error connecting to Redis:", error);
+    });
+}
+
+module.exports = connectToRedisDB;
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
-const helmet = resquire("helmet");
+const helmet = require("helmet");
 const socketio = require('socket.io');
-const connectToDatabase = require('./config/mongodb');
-
+const connectToMongoDB = require('./config/mongodb');
+const connectToRedisDB = require('./config/redisdb');
 const expressServer = app.listen(3001);
 const io = socketio(expressServer);
 
-//connect to mongodb through mongoose
-connectToDatabase();
+//connect to DBs
+connectToMongoDB();
+connectToRedisDB();
 
 //helemet.js is used for security. It prevenets XSS and ClickJacking attacks
 app.use(helmet());
@@ -19,5 +20,5 @@ app.get('/test', (req, res) => {
 
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:${PORT}');
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
