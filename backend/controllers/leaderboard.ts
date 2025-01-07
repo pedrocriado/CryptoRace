bebf755f016955b6994bf25180e7756d989c255f
pedrocriado <pedrocriadof2004@gmail.com>
2025-01-07 01:07:32 -0500

    updated the leaderboard API calls

--- backend/controllers/leaderboard.ts
+++ backend/controllers/leaderboard.ts
@@ -1,16 +1,40 @@
+import { Request, Response } from "express";
 import redis from "../config/redisdb";
 import tryCatch from "../utils/tryCatch";
+import createApiResponse from "../utils/apiResponse";
+import MessageTypes from "../utils/messageTypes";
 
-//TODO: test, document, and fully implement this three functions.
-export const addUser = tryCatch(async (username: string, personalBest: number) => {
+export const addUser = tryCatch(async (req: Request, res: Response) => {
+    const { username, personalBest } = req.body;
     await redis.zadd("Leaderboard", personalBest, username);
+    return res.status(200).json(
+        createApiResponse(true, MessageTypes.SUCCESS, 'User score added successfully!')
+    );
 });
 
-export const getLeaderboard = tryCatch(async () => {
-    return await redis.zrevrange("Leaderboard", 0, 9, "WITHSCORES");
+export const getLeaderboard = tryCatch(async (req: Request, res: Response) => {
+    const leaderboard = await redis.zrevrange("Leaderboard", 0, 9, "WITHSCORES");
+    return res.status(200).json(
+        createApiResponse(
+            true,
+            MessageTypes.SUCCESS,
+            'Leaderboard added successfully!',
+            leaderboard
+        )
+    );
 });
 
-export const getUserRank = tryCatch(async (userId: string) => {
-    const rank = await redis.zrevrank("leaderboard", userId);
-    return rank !== null ? rank + 1 : null;
+//TODO: learn wtf is wrong with this code :(
+export const getUserRank = tryCatch(async (req: Request, res: Response) => {
+    const { username } = req.body;
+    const rank = await redis.zrevrank("leaderboard", username);
+    const userRank = rank !== null ? rank + 1 : null;
+    return res.status(200).json(
+        createApiResponse(
+            true, 
+            MessageTypes.SUCCESS,
+            'User rank retrived successfully!',
+            { rank, userRank}
+        )
+    );
 });
