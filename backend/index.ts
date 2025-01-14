803b4d3bd644618b289faa8630da07679741d42e
pedrocriado <pedrocriadof2004@gmail.com>
2025-01-14 18:39:47 -0500

    making some changes to the http server

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import session from "express-session";
 import helmet from "helmet";
-import { Server } from "socket.io";
-import { createServer } from "http";
 import connectToMongoDB from "./config/mongodb";
 import rateLimiter from "./config/rateLimiter";
 import initPassport from "./config/passport";
@@ -10,10 +8,6 @@ import authRouter from "./routes/auth";
 import boardRouter from "./routes/leaderboard";
 
 const app = express();
-
-const httpServer = createServer(app);
-
-const io = new Server(httpServer);
 
 const PORT = process.env.PORT || 5000;
 
@@ -48,10 +42,8 @@ app.use(helmet());
 app.use("/", authRouter);
 app.use("/leaderboard", boardRouter);
 
-io.use((socket, next) => {
-  sessionMiddleware(socket.request as express.Request, {} as express.Response, next);
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app;
