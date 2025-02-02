0005fcb759261901a286ca7e9db770aeb9767257
pedrocriado <pedrocriadof2004@gmail.com>
2025-02-02 18:02:34 -0500

    researching socket.io client/server interactions.

---
+++ backend/socket/emits.ts
@@ -0,0 +1,4 @@
+//TODO: this file will hold all the socket.io emits 
+// that will be sent to the client.
+// eg. game winner, new messages, failed dechipher etc.
+import socketManeger from "./socketManager";
