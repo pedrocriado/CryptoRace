36da6bb7d9f219d9c13c1f759729bf3d4901ea65
pedrocriado <pedrocriadof2004@gmail.com>
2025-02-11 23:08:43 -0500

    updated getting a random sentece and key

--- backend/utils/randomCrypt.ts
+++
@@ -1,8 +0,0 @@
-const pickAlgorithm = (algortihms: [String]) => {
-    //randomly picks an element from the algortihms array
-    const randAlgo = Math.floor(Math.random() * algortihms.length);
-
-    return algortihms[randAlgo];
-}
-
-export default pickAlgorithm
